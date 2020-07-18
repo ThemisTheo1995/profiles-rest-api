0aedd31f2f7995a3e10eedc6076911b44a5a3d62
Theo <themistheodoratos@gmail.com>
2020-07-18 23:15:21 +0300

    Configured for deployment.

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(xxtv4nns+6#qi(4@onbf@rosney408vb!v3nh9&@@r!$44%k6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
